95378e5b771f577c8c33749b9cbbe0f079d23039
Denver <decsent@yandex.ru>
2017-07-09 13:00:25 +0500

    Добавлен пункт "Погода" в navigation drawler.

--- app/src/main/java/dvinc/yamblzhomeproject/MainActivity.java
+++ app/src/main/java/dvinc/yamblzhomeproject/MainActivity.java
@@ -1,6 +1,5 @@
 package dvinc.yamblzhomeproject;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,7 +11,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 /**
@@ -61,10 +59,12 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.nav_settings) {
+        if (id == R.id.nav_weather){
+
+        } else if (id == R.id.nav_settings) {
             // Настройки
         } else if (id == R.id.nav_about) {
-           showAboutDialog();
+            showAboutDialog();
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
